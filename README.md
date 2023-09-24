d5ed5fd6cdd78b630e28007ed053a56c4d617c36
asceznyk <asceznyk@gmail.com>
2023-09-24 16:50:26 +0530

    updated tutorial for channels..

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # goslow
+
+A tutorial repo for golang - all the files are named with the specific topics.
--- cmd/channels.go
+++ cmd/channels.go
@@ -1,18 +1,45 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "math/rand"
+  "time"
+)
 
-func process(c chan int) {
-  defer close(c)
-  for i :=0; i<5; i++ {
-    c <- i
+var MAX_CHICKEN_PRICE float32 = 5
+var MAX_TOFU_PRICE float32 = 3
+
+func checkPrices(wesite string, c chan string, maxPrice float32) {
+  for {
+    time.Sleep(time.Second+1);
+    var price = rand.Float32()*20
+    if price <= maxPrice { 
+      c <- wesite
+      break
+    }
+  }
+}
+
+func sendMessage(chicken chan string, tofu chan string) {
+  select {
+    case wesite := <- chicken:
+      fmt.Printf("Texted: Found a deal @ %v\n", wesite)
+    case wesite := <- tofu:
+      fmt.Printf("Emailed: Found a deal @ %v\n", wesite)
   }
 }
 
 func main() {
-  var c = make(chan int)
-  go process(c)
-  for i:= range c {
-    fmt.Println(i);
+  then := time.Now()
+  var chicken = make(chan string)
+  var tofu = make(chan string)
+  var websites = []string{"walmart.com", "costco.com", "wholefoods.com"}
+  for i := range websites {
+    go checkPrices(websites[i], chicken, MAX_CHICKEN_PRICE)
+    go checkPrices(websites[i], tofu, MAX_TOFU_PRICE)
   }
+  sendMessage(chicken, tofu)
+  fmt.Println(time.Since(then))
 }
+
+
